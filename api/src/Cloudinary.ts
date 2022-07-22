ae8bf64c4dde3943e9bbcd46c53c44975027012f
Alisson <Alisson@DESKTOP-P5MO7DJ>
2022-07-22 20:58:14 -0300

    saving repository

---
+++ api/src/Cloudinary.ts
@@ -0,0 +1,7 @@
+import cloudinary from "cloudinary";
+
+export const Cloudinary = cloudinary.v2.config({
+    cloud_name: 'poshx', 
+    api_key: '974175763915229', 
+    api_secret: 'N5eWGblUfV3Q7Pugg4GGmVhNmf0',
+});
